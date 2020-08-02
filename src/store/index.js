6d4252f896784bc8e08af8ea2c59dcdabeaecb68
Manuel Navarro <nebriand@gmail.com>
2020-08-02 22:48:56 +0200

    - Remove HelloWorld component. - Re-structure store folder. - Add Login view and add it to Router

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,4 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {}
-});
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <h1>Spotify Vue App</h1>
+    <p>Please log in with Spotify to use this app</p>
+    <a rel="noopener noreferrer" class="login__button">Login with Spotify</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  components: {}
+};
+</script>
+<style scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+.login__button {
+  cursor: pointer;
+  color: white;
+  background: green;
+  border-bottom-color: #016a01;
+  border-bottom-width: 3px;
+  border-bottom-style: solid;
+  border-radius: 5px;
+  padding: 5px 15px;
+}
+.login__button:hover {
+  background: #42b983;
+}
+</style>

63041f61c69d9a51e6a3f94e7bca2ff7382e72aa
Manuel Navarro <nebriand@gmail.com>
2020-08-03 19:51:48 +0200

    - Add number formatter filter

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+Vue.filter("numberFormatter", (value, digits = 2) => {
+  let si = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" }
+  ];
+  let rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  let i;
+  for (i = si.length - 1; i > 0; i--) {
+    if (value >= si[i].value) {
+      break;
+    }
+  }
+  return (value / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+});
+
 new Vue({
   router,
   store,

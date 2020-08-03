b92287b27a5fb124f8d75919e48256f1e62006d8
Manuel Navarro <nebriand@gmail.com>
2020-08-03 18:22:51 +0200

    - Add Artist component

---
+++ src/components/Artist.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="artist">
+    <div class="artist__img">
+      <img :src="img" />
+    </div>
+    <div class="artist__details">
+      <h1>{{ name }}</h1>
+      <h2 v-if="genres.length">{{ genres.join(", ") }}</h2>
+      <p>{{ followers }} followers</p>
+      <a :href="href" target="_blank">Listen</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Artist",
+  props: {
+    name: String,
+    genres: Array[String],
+    followers: Number,
+    href: String,
+    img: String
+  },
+  computed: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  margin: 0;
+}
+.artist {
+  flex: 0 0 calc(25% - 1em);
+  margin-bottom: 1em;
+  margin-right: 1em;
+  border: 1px solid #cccccc;
+  border-bottom-width: 4px;
+  border-bottom-color: #999999;
+  border-radius: 5px;
+  display: flex;
+}
+
+.artist__img,
+.artist__details {
+  padding: 1em;
+}
+.artist__img {
+  flex: 0 0 auto;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+.artist__details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
+.artist img {
+  width: 90px;
+  height: 90px;
+  border-radius: 90px;
+  background-color: gray;
+  object-fit: cover;
+  -o-object-position: center center;
+  object-position: center center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
     <TheHeader />
-    <div>{{ artists }}</div>
+    <div v-if="artists">
+      <h2>Artists</h2>
+      <div class="artistsList">
+        <Artist
+          v-for="artist in artists.items.slice(0, 3)"
+          :key="artist.id"
+          :name="artist.name"
+          :genres="artist.genres"
+          :img="getFirstImage(artist)"
+          :followers="artist.followers.total"
+          :href="artist.external_urls.spotify"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import TheHeader from "@/components/TheHeader";
+import Artist from "@/components/Artist";
 
 export default {
   name: "home",
   components: {
-    TheHeader
+    TheHeader,
+    Artist
+  },
+  methods: {
+    getFirstImage(item) {
+      return item.images.length > 0 ? item.images[0].url : null;
+    }
   },
   computed: {
     ...mapGetters("search", {
@@ -22,3 +42,14 @@ export default {
   }
 };
 </script>
+<style scoped>
+h2 {
+  margin-top: 2em;
+}
+.artistsList {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1em;
+  overflow-x: auto;
+}
+</style>

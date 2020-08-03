4030083f4e8e3b3190fd61cb98a09fcad18a3b67
Manuel Navarro <nebriand@gmail.com>
2020-08-03 19:52:18 +0200

    - Add BaseCard and BaseCardList components

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="baseCard">
+    <div class="baseCard__img">
+      <img :src="img" />
+    </div>
+    <div class="baseCard__details">
+      <h1 class="baseCard__title">{{ title }}</h1>
+      <h2 class="baseCard__subtitle" v-if="subtitle">{{ subtitle }}</h2>
+      <slot></slot>
+      <a :href="href" target="_blank" class="baseCard__listenButton">Listen</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+  props: {
+    title: String,
+    subtitle: Array[String],
+    href: String,
+    img: String,
+    type: String
+  },
+  computed: {}
+};
+</script>
+
+<style scoped>
+.baseCard {
+  flex: 0 0 calc(25% - 1em);
+  margin-bottom: 1em;
+  margin-right: 1em;
+  border: 1px solid var(--light-gray-color);
+  border-bottom-width: 4px;
+  border-bottom-color: var(--gray-color);
+  border-radius: 5px;
+  display: flex;
+}
+
+.baseCard__img,
+.baseCard__details {
+  padding: 1em;
+}
+.baseCard__img {
+  flex: 0 0 auto;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+.baseCard__details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
+.baseCard img {
+  width: 90px;
+  height: 90px;
+  border-radius: 90px;
+  background-color: var(--light-gray-color);
+  object-fit: cover;
+  -o-object-position: center center;
+  object-position: center center;
+}
+</style>
---
+++ src/components/BaseCardList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="baseCardList">
+    <h2 class="baseCardList__title">{{ title }}</h2>
+    <div class="baseCardList__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCardList",
+  components: {},
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+.baseCardList__title {
+  margin-top: 2em;
+}
+.baseCardList__content {
+  display: flex;
+  margin-top: 1em;
+  overflow-x: auto;
+}
+</style>

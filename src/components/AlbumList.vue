ce1bdbfbce7bb70306996bac0faededd28e2277f
Manuel Navarro <nebriand@gmail.com>
2020-08-03 19:53:09 +0200

    - Add Album and AlbumList components

---
+++ src/components/AlbumList.vue
@@ -0,0 +1,36 @@
+<template>
+  <BaseCardList title="Albums">
+    <Album
+      v-for="album in albums.items"
+      :key="album.id"
+      :name="album.name"
+      :img="getFirstImage(album)"
+      :artists="album.artists"
+      :href="album.external_urls.spotify"
+    />
+  </BaseCardList>
+</template>
+
+<script>
+import BaseCardList from "@/components/BaseCardList";
+import Album from "@/components/Album";
+
+export default {
+  name: "AlbumList",
+  components: {
+    BaseCardList,
+    Album
+  },
+  props: {
+    albums: Array[Object]
+  },
+  methods: {
+    getFirstImage(item) {
+      // TODO: Get proper image size based on device
+      return item.images.length > 0 ? item.images[0].url : null;
+    }
+  }
+};
+</script>
+
+<style scoped></style>

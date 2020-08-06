71071182f8dcd5d33efda5df207552431bc63484
Manuel Navarro <nebriand@gmail.com>
2020-08-06 23:47:47 +0200

    - Add custom scroll and transitions

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
 /* Import CSS Variables in non scoped style to make them available
 in other components and views */
 @import "./styles/variables.css";
+@import "./styles/transitions.css";
+@import "./styles/custom-scroll.css";
 body,
 html {
   margin: 0;
@@ -17,6 +19,11 @@ html {
 }
 body {
   background: #e8e8e8;
+}
+* {
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -moz-box-sizing: border-box;
 }
 #app {
   color: var(--text-color);
@@ -48,5 +55,18 @@ h2 {
 *:active,
 *:focus {
   outline-color: var(--primary-color);
+}
+/* Hide element but make it available for screen readers */
+.screen-readers {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  -webkit-clip-path: inset(50%);
+  clip-path: inset(50%);
+  border: 0;
 }
 </style>
